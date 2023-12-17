f84e438017379af083ad618985ffa0e287646942
OVasconceloss <victorolimpiovasconcelos2006@gmail.com>
2023-12-17 20:58:44 +0000

    :hammer: refactor: Organising elements and importing DescTask.tsx

--- src/components/Task/Task.tsx
+++ src/components/Task/Task.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import { DescTask } from "./DescTask/DescTask";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPencil, faTrash } from "@fortawesome/free-solid-svg-icons";
 
@@ -12,19 +14,27 @@ interface TaskProps {
 }
 
 export const Task: React.FC<TaskProps> = ({task, handleDelete}) => {
-    console.log(task)
+    const [isDescOpen, setIsDescOpen] = useState<Boolean>(false);
+
+    const openModal = () => !isDescOpen ? setIsDescOpen(true) : setIsDescOpen(false);
+
     return (
-        <div className="bg-gray-100 p-5 my-8 rounded border border-solid border-[#0000002C] flex justify-between items-center">
-            <h3 className="text-lg">{task.taskTitle}</h3>
-            <div>
-                <button 
-                className="task-button bg-red-400 hover:text-red-400 hover:border-red-400" onClick={() => handleDelete(task.id)}>
-                    <FontAwesomeIcon icon={faTrash} />
-                </button>
-                <button className="task-button bg-l-blue hover:text-l-blue hover:border-l-blue">
-                    <FontAwesomeIcon icon={faPencil} />
-                </button>
+        <>
+            <div 
+            className="bg-gray-100 p-5 my-8 rounded border border-solid border-[#0000002C] flex 
+            justify-between items-center cursor-pointer hover:border-l-blue transition duration-500 ease-in-out" onClick={openModal}>
+                <h3 className="text-lg">{task.taskTitle}</h3>
+                <div>
+                    <button 
+                    className="task-button bg-red-400 hover:text-red-400 hover:border-red-400" onClick={() => handleDelete(task.id)}>
+                        <FontAwesomeIcon icon={faTrash} />
+                    </button>
+                    <button className="task-button bg-l-blue hover:text-l-blue hover:border-l-blue">
+                        <FontAwesomeIcon icon={faPencil} />
+                    </button>
+                </div>
             </div>
-        </div>
+            {isDescOpen && <DescTask openModal={openModal} />}
+        </>
     );
 };
