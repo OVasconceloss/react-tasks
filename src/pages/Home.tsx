be8a7f589971a74ca4a92d74caf3bc00150b0c88
OVasconceloss <victorolimpiovasconcelos2006@gmail.com>
2023-12-17 15:48:24 +0000

    :sparkles: feat: Create elements for the main sidebar

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { Footer } from "../components/Footer/Footer";
 import { Header } from "../components/Header/Header";
 
@@ -5,6 +6,16 @@ const Home: React.FC = () => {
     return (
         <>
         <Header />
+        <main className="w-full h-[40.5rem]">
+            <section className="w-44 h-full p-5 bg-l-blue rounded-r-md">
+                <Link to={`/`}>
+                    <h2 className="menu-option">All Tasks</h2>
+                </Link>
+                <Link to={`/completed`}>
+                    <h2 className="menu-option">Completed Tasks</h2>
+                </Link>
+            </section>
+        </main>
         <Footer />
         </>
     );
