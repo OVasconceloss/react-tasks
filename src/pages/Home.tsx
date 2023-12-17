9b3802788b04574b959c51f0304867bda463220f
OVasconceloss <victorolimpiovasconcelos2006@gmail.com>
2023-12-17 16:41:22 +0000

    :sparkles: feat: Import and define the logic for the Modal component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,25 @@
+import { useState } from "react";
+import { Modal } from "../components/Modal/Modal";
 import { Footer } from "../components/Footer/Footer";
 import { Header } from "../components/Header/Header";
 
 const Home: React.FC = () => {
+    const [isModalOpen, setIsModalOpen] = useState<Boolean>(false);
+
+    const openModal = () => !isModalOpen ? setIsModalOpen(true) : setIsModalOpen(false);
+
     return (
         <>
         <Header />
         <main className="w-full h-[40.5rem] p-5">
             <div className="w-full justify-end text-right">
-                <button className="p-2 w-32 rounded text-white cursor-pointer border border-solid transition 
+                <button onClick={openModal} className="p-2 w-32 rounded text-white cursor-pointer border border-solid transition 
                 duration-500 ease-in-out border-white bg-l-blue hover:border-l-blue hover:bg-white hover:text-l-blue">New Task</button>
             </div>
         </main>
         <Footer />
+
+        {isModalOpen && <Modal openModal={openModal} />}
         </>
     );
 };
