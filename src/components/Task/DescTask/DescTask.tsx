67099fb853bd484e5bdf4b211943d861b3e518c2
OVasconceloss <victorolimpiovasconcelos2006@gmail.com>
2023-12-17 20:14:57 +0000

    :sparkles: feat: Create the modal to view task description

---
+++ src/components/Task/DescTask/DescTask.tsx
@@ -0,0 +1,22 @@
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+export const DescTask = () => {
+    const currentYear = new Date().getFullYear();
+    
+    return (
+        <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50">
+            <div className="bg-white w-1/2 p-8 rounded shadow-md">
+                <div className="flex flex-row justify-between">
+                    <h2 className="text-xl">""</h2>
+                    <button>
+                        <FontAwesomeIcon icon={faXmark} />
+                    </button>
+                </div>
+                <div className="text-center">
+                    <h2 className="text-[0.9rem]">Copyright &copy; {currentYear} Victor Vasconcelos</h2>
+                </div>
+            </div>
+        </div>
+    );
+};
