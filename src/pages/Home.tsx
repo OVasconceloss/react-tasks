345e9d4ada5e269028100f4c6de45ac7be5f107f
OVasconceloss <victorolimpiovasconcelos2006@gmail.com>
2023-12-17 15:52:08 +0000

    :hammer: refactor: Remove main sidebar elements

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,14 +7,6 @@ const Home: React.FC = () => {
         <>
         <Header />
         <main className="w-full h-[40.5rem]">
-            <section className="w-44 h-full p-5 bg-l-blue rounded-r-md">
-                <Link to={`/`}>
-                    <h2 className="menu-option">All Tasks</h2>
-                </Link>
-                <Link to={`/completed`}>
-                    <h2 className="menu-option">Completed Tasks</h2>
-                </Link>
-            </section>
         </main>
         <Footer />
         </>
